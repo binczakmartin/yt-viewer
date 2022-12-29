ccbc93b8f4d48bc22d401c109ec79a78d783c3e3
Martin Binczak <martin@pragmea.fr>
2022-12-29 17:49:47 +0100

    randomize browser size

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/** Stealth YT Bot with proxy support **/
 
 import { sleep, rdn, getParameterByName } from './utils.js';
 import { executablePath } from 'puppeteer';
@@ -21,23 +21,28 @@ async function createBrowser(nb, proxy) {
 
     const randomAgent = new UserAgent();
 
+    let args = [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-web-security',
+      '--user-data-dir=datadir/' + nb,
+      '--window-size=' + randomAgent.viewportHeight + ',' + randomAgent.viewportWidth,
+      '--disable-features=site-per-process',
+      '--user-agent=' + randomAgent.userAgent,
+    ]
+
+    if (process.env.USE_PROXY == 1) {
+      args.push('--proxy-server=' + proxy);
+    }
+
     const browser = await puppeteer.launch({
       defaultViewport: null,
-      headless: true,
-      args: [
-        '--proxy-server='+proxy,
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-web-security',
-        '--user-data-dir=datadir/' + nb,
-        '--window-size='+randomAgent.viewportHeight+','+randomAgent.viewportWidth,
-        '--disable-features=site-per-process',
-        '--user-agent=' + randomAgent.userAgent,
-      ],
+      headless: process.env.HEADLESS == 1 ? true : false,
+      args: args,
       ignoreHTTPSErrors: true,
       executablePath: executablePath(),
     });
-    console.log(`BROWSER ${nb} - START USING PROXY ${proxy}`);
+    console.log(`BROWSER ${nb} - START USING PROXY ${proxy ? proxy : 'NO PROXY'}`);
 
     return browser;
   } catch (e) {
@@ -68,16 +73,16 @@ async function createPage(browser) {
 
 async function getProxies() {
   const result = await axios.get(`https://api.proxyscrape.com/v2/account/datacenter_shared/proxy-list`, {
-    headers: { 
+    headers: {
       'Accept-Encoding': 'gzip,deflate,compress'
-    } ,
+    },
     params: {
       'auth': proxyApiKey,
-      'type':'getproxies',
-      'country[]':'all',
-      'protocol':'http',
-      'format':'normal',
-      'status':'all',
+      'type': 'getproxies',
+      'country[]': 'all',
+      'protocol': 'http',
+      'format': 'normal',
+      'status': 'all',
     }
   });
   return result.data.split("\r\n");
@@ -87,31 +92,88 @@ async function clickConsent(page, nb) {
   return new Promise((resolve, reject) => {
     const cookieConsentXpath = '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[2]/div/div/button';
 
-    page.waitForXPath(cookieConsentXpath, {timeout: 30000}).then(async (elem) => {
+    page.waitForXPath(cookieConsentXpath, { timeout: 2000 }).then(async (elem) => {
       const cookieConsentCoordinate = await elem.boundingBox()
       await sleep(rdn(2000, 4000));
       await page.mouse.click(cookieConsentCoordinate.x + 10, cookieConsentCoordinate.y + 10, { button: 'left' });
       console.log(`BROWSER ${nb} - CLICK CONSENT`);
       resolve();
-    }).catch((e)=>{
+    }).catch((e) => {
       console.log(`BROWSER ${nb} - CONSENT COOKIE NOT FOUND`);
       resolve();
     });
   })
 }
-
-async function clickRandomPlaylist(page, nb) {
+/* mobile other version */
+async function clickPlaylistMobile(page, nb) {
   return new Promise((resolve, reject) => {
-    const randomXpath = '//*[@id="page-manager"]/ytd-browse/ytd-playlist-header-renderer/div/div[2]/div[1]/div/div[2]/ytd-button-renderer[2]/yt-button-shape/a/yt-touch-feedback-shape/div/div[2]';
+    const randomXpath = '//*[@id="app"]/div[1]/ytm-browse/ytm-playlist-header-renderer/div[1]/a';
 
-    page.waitForXPath(randomXpath, {timeout: 30000}).then(async (elem) => {
+    page.waitForXPath(randomXpath, { timeout: 2000 }).then(async (elem) => {
       const randomCoordinate = await elem.boundingBox()
       await sleep(rdn(10000, 20000));
       console.log(`BROWSER ${nb} - CLICK RANDOM BUTTON`);
       await page.mouse.click(randomCoordinate.x + 10, randomCoordinate.y + 10, { button: 'left' });
       resolve();
-    }).catch((e) => {
-      console.log(`BROWSER ${nb} - RANDOM PLAYLIST BUTTON NOT FOUND`);
+    }).catch(() => {
+      console.log(`BROWSER ${nb} - MOBILE PLAY BUTTON NOT FOUND`);
+      resolve();
+    });
+  })
+}
+
+/* mobile */
+async function clickRandomPlaylistMobile(page, nb) {
+  return new Promise((resolve, reject) => {
+    const randomXpath = '//*[@id="app"]/div[1]/ytm-browse/ytm-playlist-header-renderer-v2/div/div[2]/div[2]/div[3]/div[2]/ytm-button-renderer/a/yt-touch-feedback-shape/div'
+
+    page.waitForXPath(randomXpath, { timeout: 2000 }).then(async (elem) => {
+      const randomCoordinate = await elem.boundingBox()
+      await sleep(rdn(10000, 20000));
+      console.log(`BROWSER ${nb} - CLICK RANDOM BUTTON`);
+      await page.mouse.click(randomCoordinate.x + 10, randomCoordinate.y + 10, { button: 'left' });
+      resolve();
+    }).catch(async () => {
+      await clickPlaylistMobile(page, nb)
+      console.log(`BROWSER ${nb} - MOBILE RANDOM PLAYLIST BUTTON NOT FOUND`);
+      resolve();
+    });
+  })
+}
+
+/* tablet */
+async function clickRandomPlaylistTablet(page, nb) {
+  return new Promise((resolve, reject) => {
+    const randomXpath = '//*[@id="top-level-buttons-computed"]/ytd-button-renderer[1]/a';
+
+    page.waitForXPath(randomXpath, { timeout: 2000 }).then(async (elem) => {
+      const randomCoordinate = await elem.boundingBox()
+      await sleep(rdn(10000, 20000));
+      console.log(`BROWSER ${nb} - CLICK RANDOM BUTTON`);
+      await page.mouse.click(randomCoordinate.x + 20, randomCoordinate.y + 20, { button: 'left' });
+      resolve();
+    }).catch(async () => {
+      console.log(`BROWSER ${nb} - TABLET RANDOM PLAYLIST BUTTON NOT FOUND`);
+      await clickRandomPlaylistMobile(page, nb);
+      resolve();
+    });
+  })
+}
+
+/* desktop */
+async function clickRandomPlaylistDektop(page, nb) {
+  return new Promise((resolve, reject) => {
+    const randomXpath = '//*[@id="page-manager"]/ytd-browse/ytd-playlist-header-renderer/div/div[2]/div[1]/div/div[2]/ytd-button-renderer[2]/yt-button-shape/a/yt-touch-feedback-shape/div/div[2]';
+
+    page.waitForXPath(randomXpath, { timeout: 2000 }).then(async (elem) => {
+      const randomCoordinate = await elem.boundingBox()
+      await sleep(rdn(10000, 20000));
+      console.log(`BROWSER ${nb} - CLICK RANDOM BUTTON`);
+      await page.mouse.click(randomCoordinate.x + 10, randomCoordinate.y + 10, { button: 'left' });
+      resolve();
+    }).catch(async () => {
+      console.log(`BROWSER ${nb} - DESKTOP RANDOM PLAYLIST BUTTON NOT FOUND`);
+      await clickRandomPlaylistTablet(page, nb);
       resolve();
     });
   })
@@ -121,7 +183,7 @@ async function clickRandomVideo(page, nb) {
   return new Promise((resolve, reject) => {
     const randomXpath = '//*[@id="top-level-buttons-computed"]/ytd-toggle-button-renderer';
 
-    page.waitForXPath(randomXpath, {timeout: 30000}).then(async (elem) => {
+    page.waitForXPath(randomXpath, { timeout: 30000 }).then(async (elem) => {
       const randomCoordinate = await elem.boundingBox()
       await sleep(rdn(10000, 20000));
       console.log(`BROWSER ${nb} - CLICK RANDOM BUTTON`);
@@ -134,33 +196,68 @@ async function clickRandomVideo(page, nb) {
   })
 }
 
+async function clickOnVideo(page, label, nb) {
+  return new Promise((resolve, reject) => {
+    const randomXpath = '//*[@id="movie_player"]/div[1]/video';
+
+    page.waitForXPath(randomXpath, { timeout: 30000 }).then(async (elem) => {
+      const randomCoordinate = await elem.boundingBox()
+      console.log(`BROWSER ${nb} - CLICK ${label} BUTTON`);
+      await page.mouse.click(randomCoordinate.x + 30, randomCoordinate.y + 30, { button: 'left' });
+      resolve();
+    }).catch((e) => {
+      console.log(`BROWSER ${nb} - ${label} BUTTON NOT FOUND`);
+      resolve();
+    });
+  })
+}
+
+async function pausePlay(page, nb) {
+  await clickOnVideo(page, 'PAUSE', nb);
+  await sleep(rdn(500, 1200));
+  await clickOnVideo(page, 'PLAY', nb);
+}
+
+async function reloadPlaylist(page, nb) {
+  const nbRload = 4;
+  const url = `https://www.youtube.com/playlist?list=${playlist_id}`;
+
+  for (let i = 0; i < nbRload; i++) {
+    console.log(`BROWSER ${nb} - RELOAD`);
+    await sleep(rdn(60000*2, 60000 * 6));
+    await page.goto(url);
+    await sleep(rdn(1000, 2200));
+    await clickRandomPlaylistDektop(page, nb);
+    await sleep(rdn(1000, 2200));
+    await sleep(rdn(1000, 2200));
+    await pausePlay(page, nb);
+  }
+}
+
 async function watchPlaylist(nb, proxy) {
   await sleep(rdn(500, 20000));
-  const nbRload = 12;
-  let url = `https://www.youtube.com/playlist?list=${playlist_id}`;
+  const url = `https://www.youtube.com/playlist?list=${playlist_id}`;
   let browser = await createBrowser(nb, proxy);
   try {
     let page = await createPage(browser);
 
-    page.on("framenavigated", frame => {
+    page.on("framenavigated", async (frame) => {
       const v = getParameterByName('v', frame.url())
       if (v) {
         console.log(`BROWSER ${nb} - WATCHING ${v}`);
+        await sleep(rdn(500, 1200));
+        await clickRandomVideo(page, nb);
+        await pausePlay(page, nb);
       }
     });
 
     await page.goto(url);
 
     await clickConsent(page, nb);
-    await clickRandomPlaylist(page, nb);
-    await clickRandomVideo(page, nb);
+    await clickRandomPlaylistDektop(page, nb);
+    // await clickRandomVideo(page, nb);
 
-    for (let i = 0; i < nbRload; i++) {
-      console.log(`BROWSER ${nb} - RELOAD`);
-      await sleep(rdn(60000, 60000*5));
-      await page.goto(url);
-      await clickRandomPlaylist(page, nb);
-    }
+    await reloadPlaylist(page, nb);
 
     console.log(`BROWSER ${nb} - CLOSE`);
     await browser.close();
@@ -181,11 +278,17 @@ async function run(nb) {
 
   try {
     process.setMaxListeners(0);
-    let proxies = await getProxies();
-
-    for (let i = 0; i < nb; i++) {
-      promiseArray.push(watchPlaylist(i, proxies[rdn(0, proxies.length-1)]));
+    if (process.env.USE_PROXY == 1) {
+      let proxies = await getProxies();
+      for (let i = 0; i < nb; i++) {
+        promiseArray.push(watchPlaylist(i, proxies[rdn(0, proxies.length - 1)]));
+      }
+    } else {
+      for (let i = 0; i < nb; i++) {
+        promiseArray.push(watchPlaylist(i));
+      }
     }
+
     await Promise.all(promiseArray);
 
     return;
